496769232febfa88daaaf1253d8d09e1783c787e
rfmo1 <rafael.pfmo@gmail.com>
2021-05-06 23:26:30 +0100

    Update agora com o comando vitórias

--- projeto.py
+++ projeto.py
@@ -49,16 +49,18 @@ def converte_tempo(tempo):
     return {"segundos": horas[2], "minutos": horas[1], "horas": horas[0],
         "dia": data[2], "mes": data[1], "ano": data[0]}
 
-
-def users_totais(lista_ano):
-    users = []
-    for jogo in lista_ano:
-        if jogo["black_username"] not in users:
-            users.append(jogo["black_username"])
-        if jogo["white_username"] not in users:
-            users.append(jogo["white_username"])
-    return users
-
+def get_jogos_user(dados):
+    info = {}
+    for dado in dados:
+        if dado["black_username"].lower() in info.keys():
+            info[dado["black_username"].lower()] += 1
+        else:
+            info[dado["black_username"].lower()] = 1
+        if dado["white_username"].lower() in info.keys():
+            info[dado["white_username"].lower()] += 1
+        else:
+            info[dado["white_username"].lower()] = 1
+    return info
 
 def tracar_anos(abcissas, jogos, jogadoras):
     fig, ax1 = plt.subplots()
@@ -72,15 +74,55 @@ def tracar_anos(abcissas, jogos, jogadoras):
     plt.title("Jogos e jogadoras por ano")
     plt.show()
 
-
 def anos(dados):
     dados_c = limpa_converte(dados, ["end_time", "black_username", "white_username"], lambda jogo: True, [lambda tempo: int(converte_tempo(tempo)["ano"]), str, str])
     conjunto_anos = list(map(lambda jogo: jogo["end_time"], dados_c))
     abcissas = range(min(conjunto_anos), max(conjunto_anos) + 1)
     filtrar_ano = lambda ano: list(filter(lambda jogo: jogo["end_time"] == ano,dados_c))
     jogos = list(map(lambda ano: len(list(filtrar_ano(ano))), abcissas))
-    jogadoras = list(map(lambda ano: len(users_totais(filtrar_ano(ano))), abcissas))
+    jogadoras = list(map(lambda ano: len(list(get_jogos_user(filtrar_ano(ano)).keys())), abcissas))
     tracar_anos(abcissas, jogos, jogadoras)
+
+def tracar_vitorias(abcissas, white_wins, black_wins, width):
+    fig, ax1 = plt.subplots()
+    X1 = range(len(abcissas))
+    X2 = list(map(lambda x: x+width, X1))
+    ax1.bar(X1, white_wins, width, color="grey")
+    ax1.set_ylabel("Percentagem de vitórias")
+    ax1.bar(X2, black_wins, width,  color="black")
+    ax1.set_xticks(X1)
+    ax1.set_xticklabels(abcissas)
+    plt.title("Percentagem de vitórias jogando com peças brancas / pretas")
+    plt.show()
+
+def vitorias_U(dados, users):
+    is_win = lambda result: result == "win"
+    users_c = list(map(lambda user: user.lower(), users))
+    dados_c = limpa_converte(dados, ["black_username", "black_result", "white_username", "white_result"], lambda jogo: jogo["white_username"].lower() in users_c or jogo["black_username"].lower() in users_c, [str, is_win, str, is_win])
+    white_wins = list(map(lambda user: len(list(filter(lambda jogo: jogo["white_username"].lower() == user and jogo["white_result"], dados_c)))/len(list(filter(lambda jogo: jogo["white_username"].lower() == user, dados_c))), users_c))
+    black_wins = list(map(lambda user: len(list(filter(lambda jogo: jogo["black_username"].lower() == user and jogo["black_result"], dados_c)))/len(list(filter(lambda jogo: jogo["black_username"].lower() == user, dados_c))), users_c))
+    tracar_vitorias(users, white_wins, black_wins, 0.3)
+
+def vitorias_C(dados, count=5):
+    is_win = lambda result: result == "win"
+    dados_c = limpa_converte(dados, ["black_username", "black_result", "white_username", "white_result"], lambda jogo: True, [str, is_win, str, is_win])
+    jogos = get_jogos_user(dados_c)
+    abcissas = list(reversed(sorted(jogos, key = jogos.get)))[:count]
+    white_wins = list(map(lambda user: len(list(filter(lambda jogo: jogo["white_username"].lower() == user and jogo["white_result"], dados_c)))/len(list(filter(lambda jogo: jogo["white_username"].lower() == user, dados_c))), abcissas))
+    black_wins = list(map(lambda user: len(list(filter(lambda jogo: jogo["black_username"].lower() == user and jogo["black_result"], dados_c)))/len(list(filter(lambda jogo: jogo["black_username"].lower() == user, dados_c))), abcissas))
+    tracar_vitorias(abcissas, white_wins, black_wins, 0.3)
+
+def vitorias(dados, opcoes):
+    if opcoes == []:
+        vitorias_C(dados)
+    elif opcoes[0] == "-c":
+        vitorias_C(dados, int(opcoes[1]))
+    elif opcoes[0] == "-u":
+        vitorias_U(dados, opcoes[1:])
+    else:
+        print("Erro: Insira opções válidas.")
+        sys.exit(1)
+
 
 
 
@@ -95,10 +137,10 @@ dados = ler_csv_dicionario_cabecalho(ficheiro)
 
 if comando == "anos":
     anos(dados)
+elif comando == "vitorias":
+    vitorias(dados, opcoes)
 '''elif comando == "classes":
     classes(dados)
-elif comando == "vitorias":
-    vitorias(dados)
 elif comando == "seguinte":
     seguinte(dados)
 elif comando == "mate":
